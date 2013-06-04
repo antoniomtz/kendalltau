8b4b9162cfd5da4efecedcbeefc7e3cf564c979b
Jose Antonio Martinez <antoniomtz86@gmail.com>
2013-06-04 00:40:19 -0600

    Update kendallTauDistance.cs

--- kendallTauDistance.cs
+++ kendallTauDistance.cs
@@ -1,13 +1,29 @@
-public int kendallTauDistance(IComparable[] a, IComparable[] b)
+public int kendallTauDistance(IComparable[] a, IComparable[] x)
         {
             int inversions = 0;
 
             for (int i = 0; i < a.Length; i++)
             {
+                    int a1 = Array.IndexOf(a,a[i]);
+                    int a2 = Array.IndexOf(a,a[j]);
+                    int x1 = Array.IndexOf(x, a[i]);
+                    int x2 = Array.IndexOf(x, a[j]);
+                   
+                    if (x2 < x1)
+                    {
+
+                        if (a[i].CompareTo(x[x2]) != 0)
+                        {
+                            int temp = x2;
+                            x2 = x1;
+                            x1 = temp;
+                        }
+                    }                    
+                    
                 for (int j = i+1; j < a.Length; j++)
                 {
-                    if (a[i].CompareTo(a[j]) != b[i].CompareTo(b[j]))                    
-                        inversions++;
+                     if (a[i].CompareTo(a[j]) != x[x1].CompareTo(x[x2]))                    
+                        inversions++;                    
                                            
                 }
             }
